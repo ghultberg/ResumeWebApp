03fcdfc80dc4a7d6a77228b43f2878fc6bc07daa
ghultberg <ghultberg@gmail.com>
2016-11-30 17:42:07 -0800

    updated resume and school with add/delete functionality

--- routes/resume_routes.js
+++ routes/resume_routes.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var resume_dal = require('../model/resume_dal');
+var account_dal = require('../model/account_dal');
 
 
 // View All schools
@@ -16,7 +17,42 @@ router.get('/all', function(req, res) {
 
 });
 
-// View the school for the given id
+router.get('/add', function(req, res){
+    // passing all the query parameters (req.query) to the insert function instead of each individually
+    account_dal.getAll(function(err,result) {
+        if (err) {
+            res.send(err);
+        }
+        else {
+            res.render('resume/resumeAdd', {'account': result});
+        }
+    });
+});
+
+router.get('/insert', function(req, res){
+    // simple validation
+    if(req.query.resume_name == null) {
+        res.send('Resume Name must be provided.');
+    }
+    else if(req.query.account_id == null) {
+        res.send('An Account must be selected');
+    }
+    else {
+
+        resume_dal.insert(req.query, function(err,result) {
+            if (err) {
+                res.send(err);
+            }
+            else {
+                //poor practice, but we will handle it differently once we start using Ajax
+                res.redirect(302, '/resume/all');
+            }
+        });
+    }
+});
+
+
+// View the resume for the given id
 router.get('/', function(req, res){
     if(req.query.resume_id == null) {
         res.send('resume_id is null');
@@ -28,6 +64,23 @@ router.get('/', function(req, res){
             }
             else {
                 res.render('resume/resumeViewById', {'result': result});
+            }
+        });
+    }
+});
+
+router.get('/delete', function(req, res){
+    if(req.query.resume_id == null) {
+        res.send('resume_id is null');
+    }
+    else {
+        resume_dal.delete(req.query.resume_id, function(err, result){
+            if(err) {
+                res.send(err);
+            }
+            else {
+                //poor practice, but we will handle it differently once we start using Ajax
+                res.redirect(302, '/resume/all');
             }
         });
     }
